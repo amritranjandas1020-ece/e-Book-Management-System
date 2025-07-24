81a961c251822254d004815b5f4b9b44c6312e07
amritranjandas1020-ece <79207844+amritranjandas1020-ece@users.noreply.github.com>
2025-07-24 22:09:28 +0530

    Add files via upload
    Full Project Push

---
+++ EbookMangement/src/main/java/com/jsp/EbookMangement/DB/DBConnect.java
@@ -0,0 +1,34 @@
+package com.jsp.EbookMangement.DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnect {
+
+	
+private  static Connection conn;
+	
+	public static  Connection getConn() {
+	
+		
+		try {
+			
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		    String url="jdbc:mysql://localhost:3306/gndec";
+		    String user="root";
+		    String pass="root";
+		
+		    conn=DriverManager.getConnection(url,user,pass);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+		
+		return conn;
+		
+	}
+	
+}
